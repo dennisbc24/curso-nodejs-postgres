02a1f21047c7cfde57590d63c322067b0ae5d8e7
dennisbc24 <dennis.bien.vida@gmail.com>
2022-06-26 22:07:59 -0500

    primer modelo con ORM

--- services/product.service.js
+++ services/product.service.js
@@ -1,11 +1,17 @@
 const faker = require('faker');
 const boom = require('@hapi/boom');
 
+//const pool = require('../libs/postgres.pool');
+const sequelize = require('../libs/sequelize');
+
 class ProductsService {
 
   constructor(){
     this.products = [];
     this.generate();
+    //this.pool = pool;
+    //this.pool.on('error', (err) => { console.error(err)})
+
   }
 
   generate() {
@@ -30,8 +36,10 @@ class ProductsService {
     return newProduct;
   }
 
-  find() {
-    return this.products;
+  async find() {
+    const query = 'SELECT * FROM tasks';
+    const [data] = await sequelize.query(query);
+    return data;
   }
 
   async findOne(id) {
--- services/user.service.js
+++ services/user.service.js
@@ -1,18 +1,22 @@
 const boom = require('@hapi/boom');
 
-const getConnection = require('../libs/postgres');
+const pool = require('../libs/postgres.pool');
+
+const { models } = require('./../libs/sequelize')
 
 class UserService {
-  constructor() {}
+  constructor() {
+    this.pool = pool;
+    this.pool.on('error', (err) => { console.error(err)})
+  }
 
   async create(data) {
     return data;
   }
 
   async find() {
-    const client = await getConnection();
-    const rta = await client.query('SELECT * FROM tasks');
-    return rta.rows;
+    const rta = await models.User.findAll();
+    return rta;
   }
 
   async findOne(id) {
