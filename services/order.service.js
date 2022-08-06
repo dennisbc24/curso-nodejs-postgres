eefd66d190cd018de9944d257fe15e3368e10a68
dennisbc24 <dennis.bien.vida@gmail.com>
2022-08-05 21:32:27 -0500

    terminando el filtro de precios

--- services/order.service.js
+++ services/order.service.js
@@ -1,32 +1,48 @@
 const boom = require('@hapi/boom');
 
+const { models } = require('./../libs/sequelize');
+
 class OrderService {
+	constructor() {
+	}
 
-  constructor(){
-  }
-  async create(data) {
-    return data;
-  }
+	async create(data) {
+		const newOrder = await models.Order.create(data);
+		return newOrder;
+	}
 
-  async find() {
-    return [];
-  }
+	async addItem(data) {
+		const newItem = await models.OrderProduct.create(data);
+		return newItem;
+	} 
 
-  async findOne(id) {
-    return { id };
-  }
+	async find() {
+		return [];
+	}
 
-  async update(id, changes) {
-    return {
-      id,
-      changes,
-    };
-  }
+	async findOne(id) {
+		const order = await models.Order.findByPk(id, {
+			include: [
+				{
+					association: 'customer',
+					include: ['user'],
+				},
+				'items'
+			],
+		});
+		return order;
+	}
 
-  async delete(id) {
-    return { id };
-  }
+	async update(id, changes) {
+		return {
+			id,
+			changes,
+		};
+	}
 
+	async delete(id) {
+		return { id };
+	}
 }
 
 module.exports = OrderService;
